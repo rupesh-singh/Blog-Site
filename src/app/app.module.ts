4a68562a3de462d3babe9902a16e35644eefa4c8
rupesh-singh <rupesh08singh08@gmail.com>
2018-04-03 22:18:15 +0530

    Add existing file2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddblogComponent } from './addblog/addblog.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+const appRoutes: Routes = [
+  {path: '' , component: HomeComponent},
+  {path: 'AddBlog' , component: AddblogComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'Login', component: LoginComponent}
+  
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AddblogComponent,
+    HomeComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
